d6e326f62dc0594e891cec99c64d5662dc67aea3
Raphael Assenat <raph@raphnet.net>
2017-01-15 15:00:23 -0500

    Version 1.0

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@
 #define VERSION_H
 
 #define PLUGIN_NAME    "Mupen64Plus raphnetraw Input Plugin"
-#define PLUGIN_VERSION           0x000904
+#define PLUGIN_VERSION           0x010000
 #define INPUT_PLUGIN_API_VERSION 0x020000
 #define CONFIG_API_VERSION       0x020100
 
