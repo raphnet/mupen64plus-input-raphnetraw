741f37cd9dae981235e57ebdc3083be5f5755439
Raphael Assenat <raph@raphnet.net>
2019-07-16 14:38:11 -0400

    Workaround against undefined SDL_PollEvents reference for windows build

--- projects/unix/buildwin64.sh
+++ projects/unix/buildwin64.sh
@@ -5,7 +5,7 @@ UNAME=MINGW64
 DESTDIR=./dist_win64
 
 HID_CFLAGS="-I $HIDHOME/hidapi"
-HID_LDLIBS="-L $HIDHOME/windows/.libs/ -lhidapi"
+HID_LDLIBS="-L $HIDHOME/windows/.libs/ -lhidapi sdl.lib"
 TARGETS=all
 MAKEFILE=Makefile
 
@@ -13,6 +13,10 @@ make -f $MAKEFILE clean
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
+# Hack to let this project link without requiring libsdl. Without
+# this linking will fail due to a missing symbol (SDL_PumpEvents)
+x86_64-w64-mingw32-dlltool -d sdl.def -l sdl.lib
 
 make -f $MAKEFILE CROSS_COMPILE=x86_64-w64-mingw32- UNAME=$UNAME HID_CFLAGS="$HID_CFLAGS" HID_LDLIBS="$HID_LDLIBS" $TARGETS V=1
 if [ $? -ne 0 ]; then
